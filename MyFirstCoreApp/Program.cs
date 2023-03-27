1bfdcf63ed1c31fdb334a6943b69de9cf770ef36
balajayapal <bala.kodis@gmail.com>
2023-03-27 17:32:40 +0530

    Add project files.

---
+++ MyFirstCoreApp/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+
+app.Run(async(HttpContext context) =>
+{
+    context.Response.Headers["MyKey"] = "Kry007";
+    context.Response.Headers["Server"] = "Server007";
+    context.Response.Headers["Content-Type"] = "text/html";
+    await context.Response.WriteAsync("<h1>Hello</h1>");
+    await context.Response.WriteAsync("<h1>World</h1>");
+}  
+    
+    );
+
+app.Run();
