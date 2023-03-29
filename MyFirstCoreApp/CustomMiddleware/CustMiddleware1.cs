81bf074911a66a6adef3f4d9de633630ea15f7d6
balajayapal <bala.kodis@gmail.com>
2023-03-29 11:08:36 +0530

    Custom middleware created.

---
+++ MyFirstCoreApp/CustomMiddleware/CustMiddleware1.cs
@@ -0,0 +1,12 @@
+﻿namespace MyFirstCoreApp.CustomMiddleware
+{
+    public class CustMiddleware1 : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("Custom Middleware");
+            await next(context);
+
+        }
+    }
+}
--- MyFirstCoreApp/Program.cs
+++ MyFirstCoreApp/Program.cs
@@ -1,4 +1,10 @@
+using MyFirstCoreApp.CustomMiddleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Registering the custom middleware service
+builder.Services.AddTransient<CustMiddleware1>();
+
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
@@ -27,6 +33,9 @@ app.Use(async (HttpContext context, RequestDelegate next) =>
     await context.Response.WriteAsync("\nMiddleware 3");
     await next(context);
 });
+
+//Adding Custom middleware
+app.UseMiddleware<CustMiddleware1>();
 
 //Middleware 4 
 //app.Use() either can act as terminating component or navigating to next
