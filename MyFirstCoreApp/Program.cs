7f68f8b501552ec5f5438f703c82589eccaccfb5
balajayapal <bala.kodis@gmail.com>
2023-03-29 10:16:45 +0530

    Middlware changed using app.Use()

--- MyFirstCoreApp/Program.cs
+++ MyFirstCoreApp/Program.cs
@@ -3,28 +3,21 @@ var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
 
-
-//app.Run() used for middleware component execution,
-//but its terminating component
-//Middleware 2 will not be executed.
+//Creating middlewares using app.Use() method
 
 //Middleware 1
-app.Run(async(HttpContext context) =>
+app.Use(async(HttpContext context,RequestDelegate next) =>
 {
-    //Genrating response headers
-    context.Response.Headers["MyKey"] = "Kry007";
-    context.Response.Headers["Server"] = "Server007";
-    context.Response.Headers["Content-Type"] = "text/html";
-    await context.Response.WriteAsync("<h1>Hello</h1>");   
-}  
-    
-);
+    await context.Response.WriteAsync("Hello\t");
+    await next(context);
 
-//Midleware 2
-app.Run(async(HttpContext context) =>
-{
-    await context.Response.WriteAsync("Bala");
 });
 
+//Middleware 2
+
+app.Use(async(HttpContext context,RequestDelegate next) => 
+{ 
+    await context.Response.WriteAsync("World");
+});    
 
 app.Run();
