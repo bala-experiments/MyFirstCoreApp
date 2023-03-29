d448d0f799bccc021f105f397008faf3e8aad0f3
balajayapal <bala.kodis@gmail.com>
2023-03-29 10:46:25 +0530

    Multiple middlewares created

--- MyFirstCoreApp/Program.cs
+++ MyFirstCoreApp/Program.cs
@@ -18,6 +18,26 @@ app.Use(async(HttpContext context,RequestDelegate next) =>
 app.Use(async(HttpContext context,RequestDelegate next) => 
 { 
     await context.Response.WriteAsync("World");
-});    
+    await next(context);
+});
+
+//Middleware 3
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("\nMiddleware 3");
+    await next(context);
+});
+
+//Middleware 4 
+//app.Use() either can act as terminating component or navigating to next
+//app.Use(async (HttpContext context, RequestDelegate next) =>
+//{
+//    await context.Response.WriteAsync("\nMiddleware 4");    
+//});
+
+app.Run(async (HttpContext context) =>
+{
+    await context.Response.WriteAsync("\nMiddleware 4");
+});
 
 app.Run();
